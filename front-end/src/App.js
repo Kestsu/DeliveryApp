1a90030ab34eb7b489f63d4a36a658fff4148209
MatheusTostes <matheus_tostes@hotmail.com>
2022-11-29 17:33:12 -0300

    feat: add all front routes

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,7 +6,17 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route exact path="/login" />
+        <Route exact path="/register" />
         <Route exact path="/customer/products" />
+        <Route exact path="/customer/products/:id" />
+        <Route exact path="/customer/products/:idVenda" />
+        <Route exact path="/customer/checkout" />
+
+        <Route exact path="/seller/order" />
+        <Route exact path="/seller/order/id" />
+
+        <Route exact path="/admin/manage" />
       </Switch>
     </BrowserRouter>
   );
---
+++ front-end/src/pages/Products.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+
+function Products() {
+  return (
+    <div>
+      <Header />
+      Products
+
+    </div>
+  );
+}
+
+export default Products;
