407d82ea2930943643a6a074ffc00fb1243cd0b8
Lucas <joselucasrs@gmail.com>
2022-11-30 18:56:10 -0300

    feat: rota de teste da autenticação criada

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const errorMiddleware = require('../middleware/errorMiddleware');
 
 const loginRouter = require('../routes/loginRouter');
+const checkRouter = require('../routes/checkRouter');
 
 const app = express();
 
@@ -11,6 +12,8 @@ app.use(express.json());
 app.get('/coffee', (_req, res) => res.status(418).end());
 
 app.use('/login', loginRouter);
+
+app.use('/check', checkRouter);
 
 app.use(errorMiddleware);
 
