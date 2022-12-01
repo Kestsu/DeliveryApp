fbd09f8799c313e16298c8235521916d92370a95
MatheusTostes <matheus_tostes@hotmail.com>
2022-12-01 19:43:30 -0300

    fix: change test url to api source

--- front-end/src/helpers/api.js
+++ front-end/src/helpers/api.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
-// const baseURL = 'http://localhost:3001/';
+const baseURL = 'http://localhost:3001/';
 
 // rota de teste
-const baseURL = 'https://virtserver.swaggerhub.com/joselucassr/AppDelivery/1.0.0/';
+// const baseURL = 'https://virtserver.swaggerhub.com/joselucassr/AppDelivery/1.0.0/';
 
 const api = axios.create({
   baseURL,
-  withCredentials: true,
-  timeout: 1000,
+  withCredentials: false,
+  timeout: 10000,
 });
 
 export default api;
--- front-end/src/pages/Manage/index.jsx
+++ front-end/src/pages/Manage/index.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from '../../components/Header';
+import api from '../../helpers/api';
 import UserForm from './components/UserForm';
 import UsersTable from './components/UsersTable';
 
 function Manage() {
   const [usersList, setUsersList] = React.useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const { data } = await api.get('/users');
+        setUsersList(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchUsers();
+  }, []);
 
   return (
     <>
