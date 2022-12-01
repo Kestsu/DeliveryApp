4610923835cebcf42e14f18202344966316b3fce
Kevin Li <Kevinstart14@gmail.com>
2022-11-30 23:27:32 -0300

    nada alterado, esta tudo comentado

--- front-end/src/components/DetailsAddress/index.jsx
+++ front-end/src/components/DetailsAddress/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
+// import { useHistory } from 'react-router-dom';
 import api from '../../helpers/api';
 
 function DetailsAddress() {
@@ -8,7 +8,7 @@ function DetailsAddress() {
   const [address, setAddress] = useState('');
   const [number, setNumber] = useState('');
   const [disabled, setDisabled] = useState(true);
-  const history = useHistory();
+  // const history = useHistory();
 
   useEffect(() => {
     (async () => {
@@ -24,9 +24,23 @@ function DetailsAddress() {
     }
   }, [seller, address, number]);
 
-  const payment = () => {
+  const payment = async () => {
     // const response = 'ID RETORNADO DO BACK'
-    history.push(`/customer/orders/${1}`);
+    // const data = await api.post('/sales', {
+    //   userId: 3,
+    //   sellerId: 2,
+    //   deliveryAddress: 'Logo alí',
+    //   deliveryNumber: '13',
+    //   products: [
+    //     {
+    //       id: 1,
+    //       quantity: 1,
+    //     },
+    //   ],
+    // });
+    // const oi = await api.get('/sales');
+    // console.log(oi.data);
+    // history.push(`/customer/orders/${1}`);
   };
 
   return (
