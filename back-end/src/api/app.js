394b6a7e4b53239731b978883b6261052333c9f1
Mila <camilapdsilva>
2022-12-02 14:57:23 -0300

    feat: liberação da pasta public para rota estática

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -15,6 +15,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/images',express.static('public/images'));
+
 app.get('/coffee', (_req, res) => res.status(418).end());
 
 app.use('/login', loginRouter);
