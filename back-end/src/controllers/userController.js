4f4fa5d5aa1e9ed9219614c1ce849388ff06daa8
allanzaparoli <allanzaparoli@gmail.com>
2022-12-06 15:16:36 -0300

    Arrumando Register

--- back-end/src/controllers/userController.js
+++ back-end/src/controllers/userController.js
@@ -4,7 +4,7 @@ const register = async (req, res) => {
   try {
     const { email, password, name } = req.body;
     const { status, result } = await userService.register({ email, password, name });
-    return res.status(status).json({ ...result });
+    return res.status(status).json({result});
   } catch (error) {
     return res.status(500).json({ message: 'Internal Server Error' });
   }
--- back-end/src/services/userService.js
+++ back-end/src/services/userService.js
@@ -1,12 +1,11 @@
 const md5 = require('md5');
 const { geraToken } = require('../utils/tokenGeneration');
 const { User } = require('../database/models');
+const { ConflictError } = require('../errors');
 
 const register = async ({ email, password, name }) => {
   const isAlreadyRegistered = await User.findOne({ where: { email } });
-  if (isAlreadyRegistered) {
-    return { status: 409, result: { message: 'Email already registered' } };
-  }
+  if (isAlreadyRegistered) throw new ConflictError('Email already registered');
   const md5password = md5(password);
   const newUser = await User.create({ name, email, password: md5password, role: 'user' });
   const token = geraToken(newUser);
