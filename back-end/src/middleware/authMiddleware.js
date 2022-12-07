09516bb7ea678355cc2a09ad983eca512a914d88
Mila <camilapdsilva>
2022-12-07 16:45:46 -0300

    fix: Corrrigdo leitura de secret

--- back-end/src/middleware/authMiddleware.js
+++ back-end/src/middleware/authMiddleware.js
@@ -1,14 +1,11 @@
 const jwt = require('jsonwebtoken');
+const secret = require('fs').readFileSync('jwt.evaluation.key', {
+  encoding: 'utf-8',
+});
 const { UnauthorizedError } = require('../errors');
 
 require('dotenv/config');
 const userService = require('../services/userService');
-
-const secret = process.env.JWT_SECRET || 'secret_key';
-
-// const secret = require('fs').readFileSync('./back-end/jwt.evaluation.key', {
-  // encoding: 'utf-8',
-// });
 
 module.exports = async (req, res, next) => {
   const token = req.header('Authorization');
